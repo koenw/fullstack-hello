ea2566c990efffd674d505d66cfedcef0cce1553
Koen Wilde <koen@chillheid.nl>
2021-11-24 19:34:13 +0100

    Make sure the import job can locate the dataset

--- mgmt/init.sh
+++ mgmt/init.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+# Make sure psql has a temp dir
 mkdir /tmp
+
+# Move the dataset(s) to where they can be found
+mv craft_beers datasets/
 
 just initdb
 just import craft_beers
