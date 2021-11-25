202d9ea6c2416a1622b8c978c75b3ba8783d4db3
Koen Wilde <koen@chillheid.nl>
2021-11-25 03:38:09 +0100

    Wait for database availability

--- mgmt/init.sh
+++ mgmt/init.sh
@@ -1,11 +1,22 @@
 #!/usr/bin/env bash
 
+function wait_for_db() {
+  echo -n "Waiting for postgres to become available..."
+  while ! nc -z "$PGHOST" "${PGPORT:5432}"; do
+    echo -n "."
+    sleep 1
+  done
+  echo " :)"
+}
+
 # Make sure psql has a temp dir
 mkdir /tmp
 
 # Move the dataset(s) to where they can be found
 mkdir -p datasets
 mv craft_beers datasets/
+
+wait_for_db
 
 just initdb
 just import craft_beers
