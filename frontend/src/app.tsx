09912042bef2a25a85e8c07467372dd89e5e148c
Koen Wilde <koen@chillheid.nl>
2021-11-25 01:00:37 +0100

    Add Beers🍻

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from "react";
-import { Configuration, Brewerie, BreweriesApi } from "./generated";
+import { Configuration, Brewerie, BreweriesApi, Beer, BeersApi } from "./generated";
 import SuperTable from "./components/table";
 import SuperTabs from "./components/tabs.tsx";
 
 import { Box, Tab, TabPanel } from '@mui/material';
 
 const breweriesApi = new BreweriesApi();
+const beersApi = new BeersApi();
 
 const App = () => {
-  const [ breweries, setBreweries ] = useState<Brewerie[]>(null);
+  const [ breweries, setBreweries ] = useState<Brewerie[]>([]);
+  const [ beers, setBeers ] = useState<Beer[]>([]);
   const [ pageIndex, setPageIndex ] = useState<number>(0);
   const [ pageSize, setPageSize ] = useState<number>(10);
   const [ order, setOrder] = useState(null);
@@ -31,56 +33,101 @@ const App = () => {
   }
 
   // TODO: Generate these from the api models to make this entirely generic
-  const columns = React.useMemo(
-    () =>[
-          {
-            Header: 'Name',
-            accessor: 'name',
-            sortDirection: 'none',
-          },
-          {
-            Header: 'City',
-            accessor: 'city',
-            sortDirection: 'none',
-          },
-          {
-            Header: 'State',
-            accessor: 'state',
-            sortDirection: 'none',
-          },
-        ],
-  );
+  const apis = [
+    {
+      id: 'breweries',
+      name: 'Breweries',
+      getMethod: (breweriesApi.breweriesGet).bind(breweriesApi),
+      setMethod: setBreweries.bind(this),
+      data: breweries,
+      columns: React.useMemo(
+        () =>[
+              {
+                Header: 'Name',
+                accessor: 'name',
+                sortDirection: 'none',
+              },
+              {
+                Header: 'City',
+                accessor: 'city',
+                sortDirection: 'none',
+              },
+              {
+                Header: 'State',
+                accessor: 'state',
+                sortDirection: 'none',
+              },
+            ],
+      ),
+    },
+    {
+      id: 'beers',
+      name: 'Beers',
+      getMethod: (beersApi.beersGet).bind(beersApi),
+      setMethod: setBeers.bind(this),
+      data: beers,
+      columns: React.useMemo(
+        () => [
+              {
+                Header: 'Name',
+                accessor: 'name',
+                sortDirection: 'none',
+              },
+              {
+                Header: 'Style',
+                accessor: 'style',
+                sortDirection: 'none',
+              },
+              {
+                Header: 'abv',
+                accessor: 'abv',
+                sortDirection: 'none',
+              },
+              {
+                Header: 'ibu',
+                accessor: 'ibu',
+                sortDirection: 'none',
+              },
+        ]
+      )
+    },
+  ];
 
   useEffect(() => {
     var args = {'limit': pageSize, 'offset': pageIndex * pageSize};
     if (order) {
       args['order'] = order;
     }
-    breweriesApi.breweriesGet(args)
+
+    apis.forEach((api) => {
+      api.getMethod(args)
       .then(
         (result) => {
-          setBreweries(result);
+          api.setMethod(result);
         },
         (error) => {
-          console.log("Failed to get breweries", error);
-        }
-      );
+          console.log(`Failed to get ${api.name}`, error);
+        },
+      )
+    });
+
   }, [pageSize, pageIndex, order]);
 
-  if (!breweries) {
-    setBreweries([]);
-  }
-
   return(
-  <SuperTabs labels={["Breweries"]}>
-    <SuperTable
-      columns={columns}
-      data={breweries}
-      initialPageSize={pageSize}
-      onChangePageIndex={setPageIndex}
-      onChangePageSize={setPageSize}
-      onHeaderClick={columnHeaderClick}
-    />
+  <SuperTabs
+    labels={apis.map((api) => api.name)}
+    >
+
+    {apis.map((api) => (
+      <SuperTable
+        columns={api.columns}
+        data={api.data}
+        initialPageSize={pageSize}
+        onChangePageIndex={setPageIndex}
+        onChangePageSize={setPageSize}
+        onHeaderClick={columnHeaderClick}
+      />
+    ))}
   </SuperTabs>
   );
 };
